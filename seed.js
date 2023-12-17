a56ecf7aab5705e3338ec595b60b482bc6410c50
bishawjitsaha <87948050+bishawjitsaha@users.noreply.github.com>
2023-12-17 17:15:48 -0500

    Update seed.js

--- seed.js
+++ seed.js
@@ -18,26 +18,26 @@ try {
     console.log('Dummy users created successfully!');
 
     // Create dummy posts
-    const post1 = await createPost('First Post', user1._id, 'Bohemian Rhapsody', 'Queen');
-    const post2 = await createPost('Second Post', user1._id, 'Hotel California', 'Eagles');
-    const post3 = await createPost('Third Post', user2._id, 'Imagine', 'John Lennon');
-    const post4 = await createPost('Fourth Post', user2._id, 'Hey Jude', 'The Beatles');
-    const post5 = await createPost('Fifth Post', user3._id, 'Bohemian Rhapsody', 'Queen');
-    const post6 = await createPost('Sixth Post', user3._id, 'Hotel California', 'Eagles');
-    const post7 = await createPost('Seventh Post', user1._id, 'Stairway to Heaven', 'Led Zeppelin');
-    const post8 = await createPost('Eighth Post', user1._id, 'Smells Like Teen Spirit', 'Nirvana');
-    const post9 = await createPost('Ninth Post', user2._id, 'Sweet Child o\' Mine', 'Guns N\' Roses');
-    const post10 = await createPost('Tenth Post', user2._id, 'Wonderwall', 'Oasis');
-    const post11 = await createPost('Eleventh Post', user3._id, 'Back in Black', 'AC/DC');
-    const post12 = await createPost('Twelfth Post', user3._id, 'November Rain', 'Guns N\' Roses');
-    const post13 = await createPost('Thirteenth Post', user1._id, 'Like a Rolling Stone', 'Bob Dylan');
-    const post14 = await createPost('Fourteenth Post', user1._id, 'Purple Haze', 'Jimi Hendrix');
-    const post15 = await createPost('Fifteenth Post', user2._id, 'Smells Like Teen Spirit', 'Nirvana');
-    const post16 = await createPost('Sixteenth Post', user2._id, 'Bohemian Rhapsody', 'Queen');
-    const post17 = await createPost('Seventeenth Post', user3._id, 'Hotel California', 'Eagles');
-    const post18 = await createPost('Eighteenth Post', user3._id, 'Imagine', 'John Lennon');
-    const post19 = await createPost('Nineteenth Post', user1._id, 'Hey Jude', 'The Beatles');
-    const post20 = await createPost('Twentieth Post', user1._id, 'Bohemian Rhapsody', 'Queen');
+    const post1 = await createPost('Bohemian Rhapsody is a rock opera', user1._id, 'Queen', 'Freddie Mercury');
+    const post2 = await createPost('Hotel California is about the dark side of the American dream', user1._id, 'Eagles', 'Don Henley');
+    const post3 = await createPost('Imagine is a peace anthem', user2._id, 'John Lennon', 'John Lennon');
+    const post4 = await createPost('Hey Jude is one of the Beatles\' longest songs', user2._id, 'The Beatles', 'Paul McCartney');
+    const post5 = await createPost('Bohemian Rhapsody has a complex vocal arrangement', user3._id, 'Queen', 'Freddie Mercury');
+    const post6 = await createPost('Hotel California is known for its iconic guitar solo', user3._id, 'Eagles', 'Don Felder');
+    const post7 = await createPost('Stairway to Heaven is often considered one of the greatest rock songs', user1._id, 'Led Zeppelin', 'Robert Plant');
+    const post8 = await createPost('Smells Like Teen Spirit is considered the anthem of Generation X', user1._id, 'Nirvana', 'Kurt Cobain');
+    const post9 = await createPost('Sweet Child o\' Mine features one of the most recognizable guitar riffs', user2._id, 'Guns N\' Roses', 'Slash');
+    const post10 = await createPost('Wonderwall is one of Oasis\' most popular songs', user2._id, 'Oasis', 'Noel Gallagher');
+    const post11 = await createPost('Back in Black is AC/DC\'s best-selling album', user3._id, 'AC/DC', 'Angus Young');
+    const post12 = await createPost('November Rain has one of the longest guitar solos in rock history', user3._id, 'Guns N\' Roses', 'Slash');
+    const post13 = await createPost('Like a Rolling Stone is considered one of Bob Dylan\'s greatest songs', user1._id, 'Bob Dylan', 'Bob Dylan');
+    const post14 = await createPost('Purple Haze is one of Jimi Hendrix\'s signature songs', user1._id, 'Jimi Hendrix', 'Jimi Hendrix');
+    const post15 = await createPost('Smells Like Teen Spirit is considered the anthem of Generation X', user2._id, 'Nirvana', 'Kurt Cobain');
+    const post16 = await createPost('Bohemian Rhapsody has a complex vocal arrangement', user2._id, 'Queen', 'Freddie Mercury');
+    const post17 = await createPost('Hotel California is known for its iconic guitar solo', user4._id, 'Eagles', 'Don Felder');
+    const post18 = await createPost('Imagine is a peace anthem', user3._id, 'John Lennon', 'John Lennon');
+    const post19 = await createPost('Hey Jude is one of the Beatles\' longest songs', user1._id, 'The Beatles', 'Paul McCartney');
+    const post20 = await createPost('Bohemian Rhapsody has a complex vocal arrangement', user4._id, 'Queen', 'Freddie Mercury');
 
     console.log('Dummy posts created successfully!');
     console.log('Done Seeding the Database!');
